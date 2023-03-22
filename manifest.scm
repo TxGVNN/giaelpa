c926fb05974acc970fc722ca1fea68ad785c2c08
Giap Tran <txgvnn@gmail.com>
2023-03-22 08:45:18 +0700

    emacs-yasnippet-snippets use latest version

--- manifest.scm
+++ manifest.scm
@@ -48,5 +48,5 @@
         "emacs-vterm"
         "emacs-vundo"
         "emacs-yasnippet"
-        "emacs-yasnippet-snippets@1.0.20230127"
+        "emacs-yasnippet-snippets"
         "emacs-ztree"))
